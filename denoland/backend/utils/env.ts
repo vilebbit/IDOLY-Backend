ed2abe4506cfb74e0d713210a557493de03c38d2
Outvi V <git@outv.im>
2026-04-26 12:13:29 +0800

    chore: switch to matomo

--- denoland/backend/utils/env.ts
+++ denoland/backend/utils/env.ts
@@ -4,15 +4,18 @@ export const MONGODB_API_KEY = Deno.env.get('MONGODB_API_KEY') ?? ''
 export const MONGODB_DATA_SOURCE = Deno.env.get('MONGODB_DATA_SOURCE') ?? ''
 export const ADMIN_TOKEN = Deno.env.get('ADMIN_TOKEN') ?? ''
 export const READONLY_TOKEN = Deno.env.get('READONLY_TOKEN') ?? ''
-export const SLACK_REPORT_URL = Deno.env.get('SLACK_REPORT_URL') ?? ''
-export const UMAMI_DOMAIN = Deno.env.get('UMAMI_DOMAIN') ?? ''
-export const UMAMI_WEBSITE_ID = Deno.env.get('UMAMI_WEBSITE_ID') ?? ''
+export const MATOMO_DOMAIN = Deno.env.get('MATOMO_DOMAIN') ?? ''
+export const MATOMO_WEBSITE_ID = Deno.env.get('MATOMO_WEBSITE_ID') ?? ''
+export const MATOMO_TOKEN = Deno.env.get('MATOMO_TOKEN') ?? ''
 export const GHTOKEN_ADV = Deno.env.get('GHTOKEN_ADV') ?? ''
 export const GHREPO_ADV = Deno.env.get('GHREPO_ADV') ?? ''
 export const MONGODB_CONNECTION = Deno.env.get('MONGODB_CONNECTION') ?? ''
 export const SENTRY_DSN = Deno.env.get('SENTRY_DSN') ?? ''
 export const BACKEND_INSTANCE_ID =
   Deno.env.get('BACKEND_INSTANCE_ID') ?? 'unknown'
+export const IS_BEHIND_CLOUDFLARE =
+  String(Deno.env.get('IS_BEHIND_CLOUDFLARE') ?? 'false').toLowerCase() ===
+  'true'
 
 /**
  * Setting env `DENO_ENV=development` will bypass all authentications.
@@ -26,14 +29,16 @@ const env = {
   MONGODB_DATA_SOURCE,
   ADMIN_TOKEN,
   READONLY_TOKEN,
-  SLACK_REPORT_URL,
-  UMAMI_DOMAIN,
-  UMAMI_WEBSITE_ID,
+
+  MATOMO_DOMAIN,
+  MATOMO_WEBSITE_ID,
+  MATOMO_TOKEN,
   GHTOKEN_ADV,
   GHREPO_ADV,
   MONGODB_CONNECTION,
   SENTRY_DSN,
   BACKEND_INSTANCE_ID,
+  IS_BEHIND_CLOUDFLARE,
 }
 
 export default env
--- denoland/backend/utils/ping.ts
+++ denoland/backend/utils/ping.ts
@@ -1,28 +1,37 @@
 import { Request } from '@oak/oak'
-import { UMAMI_DOMAIN, UMAMI_WEBSITE_ID } from './env.ts'
+import {
+  IS_BEHIND_CLOUDFLARE,
+  MATOMO_DOMAIN,
+  MATOMO_TOKEN,
+  MATOMO_WEBSITE_ID,
+} from './env.ts'
 
 export default async function ping(
   req: Request,
   remoteAddr: string
 ): Promise<void> {
-  const url = new URL(req.url)
-  await fetch(`https://${UMAMI_DOMAIN}/api/send`, {
-    body: JSON.stringify({
-      payload: {
-        website: UMAMI_WEBSITE_ID,
-        url: url.pathname + url.search,
-        referrer: req.headers.get('Referer'),
-        hostname: url.hostname,
-        language: req.headers.get('Accept-Language'),
-        screen: '1920x1080',
-      },
-      type: 'event',
-    }),
-    headers: {
-      'User-Agent': req.headers.get('User-Agent') ?? '',
-      'Content-Type': 'application/json',
-      'X-Forwarded-For': remoteAddr,
-    },
+  const requestUrl = new URL(req.url)
+  const pingUrl = new URL(`https://${MATOMO_DOMAIN}/matomo.php`)
+  const payload = {
+    idsite: MATOMO_WEBSITE_ID,
+    rec: 1,
+    url: String(requestUrl),
+    rand: String(Math.random()),
+    apiv: 1,
+
+    urlref: req.headers.get('Referer'),
+    ua: req.headers.get('User-Agent'),
+    lang: req.headers.get('Accept-Language'),
+    token_auth: MATOMO_TOKEN,
+    cip: IS_BEHIND_CLOUDFLARE
+      ? req.headers.get('Cf-Connecting-Ip')
+      : remoteAddr,
+    country: IS_BEHIND_CLOUDFLARE
+      ? (req.headers.get('Cf-Ipcountry') ?? '').toLowerCase()
+      : '',
+  }
+  Object.entries(payload).forEach(([k, v]) => pingUrl.searchParams.set(k, v))
+  await fetch(pingUrl, {
     method: 'POST',
   }).then((x) => x.text())
 }
